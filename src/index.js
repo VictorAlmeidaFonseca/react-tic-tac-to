a2ad88b489c66273872668fcdc5f8255ef8cdeec
VictorAlmeidaFonseca <fonseca.almeida.victor@gmail.com>
2020-04-26 17:51:26 -0300

    Add isNext state

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-            className="square"
-            onClick={ () => this.props.onclick() } //Poderia ser qualuqe nome passado como props, porém existe uma convenção
-            >
-            {this.props.value}
-        </button>
-      );
-    }
+function Square(props) {
+  return (
+    <button 
+      className="square"
+      onClick={props.onClick}
+      >
+      {props.value}
+    </button>
+     );
 }
   
 class Board extends React.Component {
@@ -21,23 +19,27 @@ class Board extends React.Component {
       super(props);
       this.state = {
          squares: Array(9).fill(null),
+         xIsNext: true,
       };
   }
 
   handleClick(i) {
       const squares = this.state.squares.slice();
-      squares[1] = 'X';
-      this.setState({squares: squares})
-  }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xISNext: !this.state.xIsNext,
+        });
+    }
 
   renderSquare(i) {
     return <Square value={this.state.squares[i]}
-            onclick={() => this.handleClick(i)}
+            onClick={() => this.handleClick(i)}
           />;
   }
   
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); 
   
     return (
       <div>
